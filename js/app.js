37460436edf560081c8e3d778e3c9b6ba3b5261a
Abeeb Ridwan <olumideuae@gmail.com>
2018-06-21 08:09:06 +0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,10 +1,1 @@
-var viewModel = function(){
-    this.catName = ko.observable("Shy Cat");
-    this.imgSrc = ko.observable("https://www.our-happy-cat.com/images/xshy-hiding-cat.jpg.pagespeed.ic.aGWMLHO7sn.jpg");
-    this.clickCount = ko.observable(0);
-    this.incrementCounter =  function(){
-              this.clickCount( this.clickCount()+1)    
-    }
-};
 
-ko.applyBidings(new viewModel());
